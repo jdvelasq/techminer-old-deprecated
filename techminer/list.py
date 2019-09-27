e0983be7d1981bcd7fc59b04d6bf4ab7af234a25
jdvelasq <jdvelasq@unal.edu.co>
2019-09-27 09:21:19 -0500

    nueva version documentacion

--- techminer/list.py
+++ techminer/list.py
@@ -53,14 +53,52 @@ class List(pd.DataFrame):
             alt.X(columns[1] + ':Q'),
             alt.Color(columns[1] + ':Q', scale=alt.Scale(scheme=color)))
 
+    #----------------------------------------------------------------------------------------------
+    def altair_barplot(self):
+        """Vertical bar plot in Altair.
+
+        >>> import pandas as pd
+        >>> import matplotlib.pyplot as plt
+        >>> from techminer.dataframe import  *
+        >>> rdf = RecordsDataFrame(
+        ...     pd.read_json('./data/cleaned.json', orient='records', lines=True)
+        ... )
+        >>> rdf.documents_by_year().altair_barplot()
+
+        .. image:: ../figs/altair_barplot.jpg
+            :width: 500px
+            :align: center                
+        """
+
+        columns = self.columns.tolist()
+        data = List(self.copy())
+        data[columns[0]] = data[columns[0]].map(str) + ' [' + data[columns[1]].map(str) + ']'
+        data[data.columns[0]] = data[data.columns[0]].map(lambda x: cut_text(x))
+
+        alt.Chart(data).mark_bar().encode(
+            alt.X(columns[0] + ':N', sort=alt.EncodingSortField(field=columns[1] + ':Q')),
+            alt.Y(columns[1] + ':Q'),
+            alt.Color(columns[1] + ':Q', scale=alt.Scale(scheme='greys')))
 
 
     #----------------------------------------------------------------------------------------------
-    def barhplot(self, library=None, color=None):
+    def barhplot(self, color=None, figsize=(12,8)):
         """Plots a pandas.DataFrame using Altair.
 
+        >>> import pandas as pd
+        >>> import matplotlib.pyplot as plt
+        >>> from techminer.dataframe import  *
+        >>> rdf = RecordsDataFrame(
+        ...     pd.read_json('./data/cleaned.json', orient='records', lines=True)
+        ... )
+        >>> rdf.documents_by_year().barhplot()
 
+        .. image:: ../figs/barhplot.jpg
+            :width: 600px
+            :align: center   
         """
+
+        plt.figure(figsize=figsize)
 
         columns = self.columns.tolist()
         data = List(self.copy())
@@ -81,49 +119,31 @@ class List(pd.DataFrame):
 
 
     #----------------------------------------------------------------------------------------------
-    def barplot(self, library=None):
-        """Vertical bar plot in Altair.
+    def barplot(self, color='gray', figsize=(8,12)):
+        """Vertical bar plot in matplotlib.
+
+        >>> import pandas as pd
+        >>> import matplotlib.pyplot as plt
+        >>> from techminer.dataframe import  *
+        >>> rdf = RecordsDataFrame(
+        ...     pd.read_json('./data/cleaned.json', orient='records', lines=True)
+        ... )
+        >>> rdf.documents_by_year().barplot()
+
+        .. image:: ../figs/barplot.jpg
+            :width: 600px
+            :align: center                
+        
         """
 
         columns = self.columns.tolist()
 
+        plt.figure(figsize=figsize)
         data = List(self.copy())
         data[columns[0]] = data[columns[0]].map(str) + ' [' + data[columns[1]].map(str) + ']'
         data[data.columns[0]] = data[data.columns[0]].map(lambda x: cut_text(x))
-
-        if library is None:
-            data.plot.bar(columns[0], columns[1], color='gray')
-            plt.gca().yaxis.grid(True)
-            return
-
-        if library == 'altair':
-        
-            return alt.Chart(data).mark_bar().encode(
-                alt.X(columns[0] + ':N', sort=alt.EncodingSortField(field=columns[1] + ':Q')),
-                alt.Y(columns[1] + ':Q'),
-                alt.Color(columns[1] + ':Q', scale=alt.Scale(scheme='greys')))
-
-        if library == 'seaborn':
-            columns = data.columns.tolist()
-            result = sns.barplot(
-                y=columns[1],
-                x=columns[0],
-                data=data,
-                label=columns[0],
-                color="gray")
-            _, labels = plt.xticks()
-            result.set_xticklabels(labels, rotation=90)
-            plt.gca().yaxis.grid(True)
-            return
-
-
-
-
-
-
-
-
-
+        data.plot.bar(columns[0], columns[1], color=color)
+        plt.gca().yaxis.grid(True)
 
     #----------------------------------------------------------------------------------------------
     def print_IDs(self):
@@ -170,6 +190,40 @@ class List(pd.DataFrame):
         plt.gca().xaxis.grid(True)
 
     #----------------------------------------------------------------------------------------------
+    def seaborn_barplot(self, color='gray'):
+        """Vertical bar plot in Seaborn.
+
+        >>> import pandas as pd
+        >>> import matplotlib.pyplot as plt
+        >>> from techminer.dataframe import  *
+        >>> rdf = RecordsDataFrame(
+        ...     pd.read_json('./data/cleaned.json', orient='records', lines=True)
+        ... )
+        >>> rdf.documents_by_year().altair_barplot()
+
+        .. image:: ../figs/seaborn_barhplot.jpg
+            :width: 800px
+            :align: center                
+        """
+
+        columns = self.columns.tolist()
+        data = List(self.copy())
+        data[columns[0]] = data[columns[0]].map(str) + ' [' + data[columns[1]].map(str) + ']'
+        data[data.columns[0]] = data[data.columns[0]].map(lambda x: cut_text(x))
+
+        columns = data.columns.tolist()
+        result = sns.barplot(
+            y=columns[1],
+            x=columns[0],
+            data=data,
+            label=columns[0],
+            color=color)
+        _, labels = plt.xticks()
+        result.set_xticklabels(labels, rotation=90)
+        plt.gca().yaxis.grid(True)
+
+
+    #----------------------------------------------------------------------------------------------
     def title_view(self, column_values=None):
         """
         """
@@ -177,7 +231,22 @@ class List(pd.DataFrame):
 
     #----------------------------------------------------------------------------------------------
     def wordcloud(self, figsize=(14, 7), max_font_size=50, max_words=100, 
-        background_color="white"):
+            background_color="white"):
+        """
+
+
+        >>> import pandas as pd
+        >>> import matplotlib.pyplot as plt
+        >>> from techminer.dataframe import  *
+        >>> rdf = RecordsDataFrame(
+        ...     pd.read_json('./data/cleaned.json', orient='records', lines=True)
+        ... )
+        >>> rdf.documents_by_terms('Source title').wordcloud()
+
+        .. image:: ../figs/wordcloud.jpg
+            :width: 800px
+            :align: center                
+        """
         
 
         columns = self.columns.tolist()
@@ -197,6 +266,28 @@ class List(pd.DataFrame):
     #----------------------------------------------------------------------------------------------
     def worldmap(self, figsize=(14, 7)):
         """Worldmap plot with the number of documents per country.
+
+        >>> import pandas as pd
+        >>> import matplotlib.pyplot as plt
+        >>> from techminer.dataframe import  *
+        >>> from techminer.strings import  *
+        >>> rdf = RecordsDataFrame(
+        ...     pd.read_json('./data/cleaned.json', orient='records', lines=True)
+        ... )
+        >>> rdf['Country'] = rdf['Affiliations'].map(lambda x: extract_country(x, sep=';'))
+        >>> rdf.documents_by_terms('Country', sep=';').head()
+                  Country  Num Documents                                                 ID
+        0           China             83  [[*3*], [*4*], [*6*], [*6*], [*7*], [*10*], [*...
+        1          Taiwan             20  [[*14*], [*14*], [*17*], [*17*], [*17*], [*17*...
+        2   United States             17  [[*3*], [*22*], [*23*], [*23*], [*26*], [*26*]...
+        3  United Kingdom             15  [[*5*], [*7*], [*11*], [*11*], [*11*], [*28*],...
+        4           India             15  [[*9*], [*50*], [*51*], [*56*], [*56*], [*57*]...
+
+        >>> rdf.documents_by_terms('Country', sep=';').worldmap()
+
+        .. image:: ../figs/worldmap.jpg
+            :width: 800px
+            :align: center
         """
 
         if 'Country' not in list(self.columns):
